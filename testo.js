d458934085d3dc02bbd320ea966b701a0049f665
Tincho1515 <65197963+Tincho1515@users.noreply.github.com>
2023-04-08 21:43:36 -0300

    Update testo.js

--- testo.js
+++ testo.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  const today = new Date().toLocaleDateString();
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`<h1>La fecha actual es ${today}</h1>`);
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Mensajes cada 10 segundos:\n');
+  setInterval(() => {
+    res.write('¡Hola! Este mensaje se envía cada 10 segundos.\n');
+  }, 10000);
 });
 
-server.listen(3000, () => {
-  console.log('Servidor web iniciado en el puerto 3000');
-});
+server.listen(8080);
+console.log('Servidor escuchando en el puerto 8080');
