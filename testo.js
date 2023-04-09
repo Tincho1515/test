e2bdf66fc2ad7f07c429815f626cf94f521887c5
Tincho1515 <65197963+Tincho1515@users.noreply.github.com>
2023-04-08 21:10:54 -0300

    Update testo.js

--- testo.js
+++ testo.js
@@ -1,11 +1,12 @@
-const WebSocket = require('ws');
+const http = require('http');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = http.createServer((req, res) => {
+  const today = new Date().toLocaleDateString();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(`<h1>La fecha actual es ${today}</h1>`);
+  res.end();
+});
 
-server.on('connection', (socket) => {
-    console.log('Cliente conectado');
-
-    setInterval(() => {
-        socket.send('Mensaje test wss');
-    }, 1000);
+server.listen(3000, () => {
+  console.log('Servidor web iniciado en el puerto 3000');
 });
